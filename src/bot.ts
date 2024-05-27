f60f59584f7b644611babeaedacd7215de4aa731
iceknight17 <iceknight@gmail.com>
2024-05-27 10:12:52 -0500

    finished bill page

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,6 @@
 import { Bot, InputFile } from "grammy";
 import puppeteer from "puppeteer";
+import fs from "node:fs";
 
 // Create a bot using the Telegram token
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
@@ -15,16 +16,18 @@ const genInvoice = async () => {
     await page.setViewport({ width: 500, height: 1080, isMobile: true })
   
     // Your HTML code
-    const html = `
-      <html>
-        <body>
-          <h1>Hello, World!</h1>
-        </body>
-      </html>
-    `
+    const invoiceHtml = await fs.readFileSync('invoiceHtmls/index.html', 'utf-8');
+    console.log('invoiceHtml', invoiceHtml);
+    // const html = `
+    //   <html>
+    //     <body>
+    //       <h1>Hello, World!</h1>
+    //     </body>
+    //   </html>
+    // `
   
     // Set the HTML content of the page
-    await page.setContent(html)
+    await page.setContent(invoiceHtml)
   
     // Take a screenshot of the page
     const invoiceImg = await page.screenshot({ path: 'exports/screenshot.png' })
@@ -35,8 +38,9 @@ const genInvoice = async () => {
 }
 
 const replyWithPhoto = async (ctx: any) => {
+    console.log('reply-with-photo');
     const invoiceImg = await genInvoice();
-    console.log('invoiceImg', invoiceImg);
+    console.log('invoiceHtml', invoiceImg);
     ctx.replyWithPhoto(new InputFile(invoiceImg));
 }
 
